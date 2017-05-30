aaf566dc1c66667ba5d8744ac38b9ff42e11a1d9
Shi Hung <c.shihhung@gmail.com>
2017-05-30 15:29:14 +0000

    Compleated api project

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/api/whoami', function(req, res){
+    var ipaddress = req.headers['x-forwarded-for']
+    var language = req.headers['accept-language'].split(';')[0]
+    var full_user_agent = req.headers['user-agent']
+    
+    //extract os from req.headers['user-agent']
+    var expression = /\([a-zA-Z\d\s\.\;]+\)/gi
+    var software = full_user_agent.match(expression)[0]
+    
+    res.writeHeader(200,{'Content-Type':'text/plain'})
+    res.end(JSON.stringify({ 'ipaddress': ipaddress, 'language': language, 'software': software }))
+})
+
+app.get('*',function(req, res){
+    res.writeHeader(404,{'Content-Type':'text/plain'})
+    res.end('404 error! it is not supported operation')
 })
 
 app.listen(8080, function () {
