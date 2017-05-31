d7c3e1a08447ceaa0e0129124c41cb9a625bd209
Shi Hung <c.shihhung@gmail.com>
2017-05-31 13:39:27 +0000

    Completed request header parser api project

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express()
 
 app.get('/api/whoami', function(req, res){
     var ipaddress = req.headers['x-forwarded-for']
-    var language = req.headers['accept-language'].split(';')[0]
+    var language = req.headers['accept-language'].split(',')[0]
     var full_user_agent = req.headers['user-agent']
     
     //extract os from req.headers['user-agent']
     var expression = /\([a-zA-Z\d\s\.\;]+\)/gi
     var software = full_user_agent.match(expression)[0]
+    software = software.toString().replace('(','')
+    software = software.toString().replace(')','')
     
     res.writeHeader(200,{'Content-Type':'text/plain'})
     res.end(JSON.stringify({ 'ipaddress': ipaddress, 'language': language, 'software': software }))
